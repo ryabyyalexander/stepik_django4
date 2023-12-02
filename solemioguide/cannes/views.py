8dac4ec7422820d1c8bf369b9c21ea80f0f7367a
Oleksandr Riabyi <ryabyy.alexander@gmail.com>
2023-12-02 09:22:42 +0200

    urls

--- solemioguide/cannes/views.py
+++ solemioguide/cannes/views.py
@@ -1,9 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 
 def index(request):
-    return HttpResponse("Страница приложения cannes")
+    return HttpResponse("<h1>Страница приложения cannes</h1>")
 
 
 def categories(request, cat_id):
@@ -17,4 +16,10 @@ def categories_by_slug(request, cat_slug):
 
 
 def archive(request, year):
+    if year > 2023:
+        raise Http404()
     return HttpResponse(f"<h3>Статьи по годам</h3><h1>{year}</h1>")
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
